c0188385cf8f3c61dd004ebd16985c51b8243b9f
alexa-whitney <alexawhitney@gmail.com>
2023-08-16 12:59:25 -0600

    Challenge 4 Completed

--- src/index.js
+++ src/index.js
@@ -6,4 +6,23 @@ function allCaps(str) {
     return str.toUpperCase();
 }
 
-console.log(allCaps('hello world'));
+function capitalizeWords(str) {
+    return str.split(' ').map(capitalize).join(' ');
+}
+
+console.log(capitalizeWords('hello world, my name is alexa and i am not a robot'))
+
+function capitalizeHeadline(str) {
+    const exceptions = ['the', 'in', 'a', 'an', 'and', 'but', 'for', 'at', 'by', 'from'];
+    return str.split(' ').map((word, index) => {
+        if (index === 0) {
+            return capitalize(word);
+        } else if (exceptions.includes(word)) {
+            return word;
+        } else {
+            return capitalize(word);
+        }
+    }).join(' '); 
+}
+
+console.log(capitalizeHeadline('the cat and the dog sat by the window in order to watch the rain fall'))
